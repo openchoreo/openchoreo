4857a2e0cc16b7a2a6ee51ef49ec6ca5fe3f0796
Stefinie Fernando <minolispencer@gmail.com>
2025-10-15 11:08:07 +0530

    Remove unused styles from LogoIcon component

--- packages/app/src/components/Root/LogoIcon.tsx
+++ packages/app/src/components/Root/LogoIcon.tsx
@@ -1,15 +1,4 @@
-import { makeStyles } from '@material-ui/core';
 import { OpenChoreoIcon } from '@openchoreo/backstage-design-system';
-
-const useStyles = makeStyles({
-  svg: {
-    width: 'auto',
-    height: 28,
-  },
-  path: {
-    fill: '#7df3e1',
-  },
-});
 
 const LogoIcon = () => {
 
