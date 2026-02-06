yashodgayashan/open-choreo

feat(controller): add cluster level dataplanes, buildplanes and observabilityplanes
Signed-off-by: yashodgayashan <yashodgayashan@gmail.com>

api/v1alpha1/clusterbuildplane_types.go
@@ -1,0 +1,92 @@
+// Copyright 2025 The OpenChoreo Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ClusterBuildPlaneSpec defines the desired state of ClusterBuildPlane.
+// This is a cluster-scoped version of BuildPlaneSpec, allowing platform admins
+// to define build planes that can be referenced across namespaces.
+type ClusterBuildPlaneSpec struct {
+	// PlaneID identifies the logical plane this CR connects to.
+	// Multiple ClusterBuildPlane CRs can share the same planeID to connect to the same physical cluster
+	// while maintaining separate configurations for multi-tenancy scenarios.
+	// If not specified, defaults to the CR name for backwards compatibility.
+	// Format: lowercase alphanumeric characters, hyphens allowed, max 63 characters.
+	// Examples: "shared-builder", "ci-cluster", "us-west-2"
+	// +optional
+	// +kubebuilder:validation:MaxLength=63
+	// +kubebuilder:validation:Pattern=^[a-z0-9]([-a-z0-9]*[a-z0-9])?$
+	PlaneID string `json:"planeID,omitempty"`
+
+	// ClusterAgent specifies the configuration for cluster agent-based communication
+	// The cluster agent establishes a WebSocket connection to the control plane's cluster gateway
+	// This field is mandatory - all build planes must use cluster agent communication
+	ClusterAgent ClusterAgentConfig `json:"clusterAgent"`
+
+	// SecretStoreRef specifies the ESO ClusterSecretStore to use in the build plane
+	// +optional
+	SecretStoreRef *SecretStoreRef `json:"secretStoreRef,omitempty"`
+
+	// ClusterObservabilityPlaneRef specifies the name of the ClusterObservabilityPlane for this ClusterBuildPlane.
+	// This references a cluster-scoped ObservabilityPlane resource.
+	// +optional
+	ClusterObservabilityPlaneRef string `json:"clusterObservabilityPlaneRef,omitempty"`
+}
+
+// ClusterBuildPlaneStatus defines the observed state of ClusterBuildPlane.
+type ClusterBuildPlaneStatus struct {
+	// ObservedGeneration reflects the generation of the most recently observed ClusterBuildPlane.
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
+	// Conditions represent the current state of the ClusterBuildPlane resource.
+	// +listType=map
+	// +listMapKey=type
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// AgentConnection tracks the status of cluster agent connections to this build plane
+	// +optional
+	AgentConnection *AgentConnectionStatus `json:"agentConnection,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster,shortName=cbp;cbps
+
+// ClusterBuildPlane is the Schema for the clusterbuildplanes API.
+// It is a cluster-scoped version of BuildPlane, allowing platform administrators
+// to define build plane configurations that can be referenced across multiple namespaces.
+type ClusterBuildPlane struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterBuildPlaneSpec   `json:"spec,omitempty"`
+	Status ClusterBuildPlaneStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ClusterBuildPlaneList contains a list of ClusterBuildPlane.
+type ClusterBuildPlaneList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterBuildPlane `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ClusterBuildPlane{}, &ClusterBuildPlaneList{})
+}
+
+// GetConditions returns the conditions of the ClusterBuildPlane.
+func (c *ClusterBuildPlane) GetConditions() []metav1.Condition {
+	return c.Status.Conditions
+}
+
+// SetConditions sets the conditions of the ClusterBuildPlane.
+func (c *ClusterBuildPlane) SetConditions(conditions []metav1.Condition) {
+	c.Status.Conditions = conditions
+}

internal/controller/clusterbuildplane/controller_test.go
@@ -1,0 +1,71 @@
+// Copyright 2025 The OpenChoreo Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package clusterbuildplane
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	openchoreov1alpha1 "github.com/openchoreo/openchoreo/api/v1alpha1"
+)
+
+var _ = Describe("ClusterBuildPlane Controller", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		clusterbuildplane := &openchoreov1alpha1.ClusterBuildPlane{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind ClusterBuildPlane")
+			err := k8sClient.Get(ctx, typeNamespacedName, clusterbuildplane)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &openchoreov1alpha1.ClusterBuildPlane{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					// TODO(user): Specify other spec details if needed.
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &openchoreov1alpha1.ClusterBuildPlane{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance ClusterBuildPlane")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &ClusterBuildPlaneReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})

internal/controller/clusterdataplane/controller_test.go
@@ -1,0 +1,71 @@
+// Copyright 2025 The OpenChoreo Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package clusterdataplane
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	openchoreov1alpha1 "github.com/openchoreo/openchoreo/api/v1alpha1"
+)
+
+var _ = Describe("ClusterDataPlane Controller", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		clusterdataplane := &openchoreov1alpha1.ClusterDataPlane{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind ClusterDataPlane")
+			err := k8sClient.Get(ctx, typeNamespacedName, clusterdataplane)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &openchoreov1alpha1.ClusterDataPlane{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					// TODO(user): Specify other spec details if needed.
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &openchoreov1alpha1.ClusterDataPlane{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance ClusterDataPlane")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &ClusterDataPlaneReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})

internal/controller/clusterobservabilityplane/controller_test.go
@@ -1,0 +1,71 @@
+// Copyright 2025 The OpenChoreo Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package clusterobservabilityplane
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	openchoreov1alpha1 "github.com/openchoreo/openchoreo/api/v1alpha1"
+)
+
+var _ = Describe("ClusterObservabilityPlane Controller", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		clusterobservabilityplane := &openchoreov1alpha1.ClusterObservabilityPlane{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind ClusterObservabilityPlane")
+			err := k8sClient.Get(ctx, typeNamespacedName, clusterobservabilityplane)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &openchoreov1alpha1.ClusterObservabilityPlane{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					// TODO(user): Specify other spec details if needed.
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &openchoreov1alpha1.ClusterObservabilityPlane{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance ClusterObservabilityPlane")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &ClusterObservabilityPlaneReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})

openchoreo/openchoreo

api/v1alpha1/clusterbuildplane_types.go
@@ -1,0 +1,93 @@
+// Copyright 2025 The OpenChoreo Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ClusterBuildPlaneSpec defines the desired state of ClusterBuildPlane.
+// This is a cluster-scoped version of BuildPlaneSpec, allowing platform admins
+// to define build planes that can be referenced across namespaces.
+type ClusterBuildPlaneSpec struct {
+	// PlaneID identifies the logical plane this CR connects to.
+	// Multiple ClusterBuildPlane CRs can share the same planeID to connect to the same physical cluster
+	// while maintaining separate configurations for multi-tenancy scenarios.
+	// This field is required and must uniquely identify the physical cluster agent connection.
+	// Format: lowercase alphanumeric characters, hyphens allowed, max 63 characters.
+	// Examples: "shared-builder", "ci-cluster", "us-west-2"
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:MaxLength=63
+	// +kubebuilder:validation:Pattern=^[a-z0-9]([-a-z0-9]*[a-z0-9])?$
+	PlaneID string `json:"planeID"`
+
+	// ClusterAgent specifies the configuration for cluster agent-based communication
+	// The cluster agent establishes a WebSocket connection to the control plane's cluster gateway
+	// This field is mandatory - all build planes must use cluster agent communication
+	ClusterAgent ClusterAgentConfig `json:"clusterAgent"`
+
+	// SecretStoreRef specifies the ESO ClusterSecretStore to use in the build plane
+	// +optional
+	SecretStoreRef *SecretStoreRef `json:"secretStoreRef,omitempty"`
+
+	// ObservabilityPlaneRef specifies the name of the ClusterObservabilityPlane for this ClusterBuildPlane.
+	// Since this is a cluster-scoped resource, it can only reference other cluster-scoped resources.
+	// This field must reference a ClusterObservabilityPlane resource, not a namespace-scoped ObservabilityPlane.
+	// +optional
+	ObservabilityPlaneRef string `json:"observabilityPlaneRef,omitempty"`
+}
+
+// ClusterBuildPlaneStatus defines the observed state of ClusterBuildPlane.
+type ClusterBuildPlaneStatus struct {
+	// ObservedGeneration reflects the generation of the most recently observed ClusterBuildPlane.
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
+	// Conditions represent the current state of the ClusterBuildPlane resource.
+	// +listType=map
+	// +listMapKey=type
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// AgentConnection tracks the status of cluster agent connections to this build plane
+	// +optional
+	AgentConnection *AgentConnectionStatus `json:"agentConnection,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster,shortName=cbp;cbps
+
+// ClusterBuildPlane is the Schema for the clusterbuildplanes API.
+// It is a cluster-scoped version of BuildPlane, allowing platform administrators
+// to define build plane configurations that can be referenced across multiple namespaces.
+type ClusterBuildPlane struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterBuildPlaneSpec   `json:"spec,omitempty"`
+	Status ClusterBuildPlaneStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ClusterBuildPlaneList contains a list of ClusterBuildPlane.
+type ClusterBuildPlaneList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterBuildPlane `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ClusterBuildPlane{}, &ClusterBuildPlaneList{})
+}
+
+// GetConditions returns the conditions of the ClusterBuildPlane.
+func (c *ClusterBuildPlane) GetConditions() []metav1.Condition {
+	return c.Status.Conditions
+}
+
+// SetConditions sets the conditions of the ClusterBuildPlane.
+func (c *ClusterBuildPlane) SetConditions(conditions []metav1.Condition) {
+	c.Status.Conditions = conditions
+}

internal/controller/clusterbuildplane/controller_test.go
@@ -1,0 +1,70 @@
+// Copyright 2025 The OpenChoreo Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package clusterbuildplane
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	openchoreov1alpha1 "github.com/openchoreo/openchoreo/api/v1alpha1"
+)
+
+var _ = Describe("ClusterBuildPlane Controller", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name: resourceName,
+		}
+		clusterbuildplane := &openchoreov1alpha1.ClusterBuildPlane{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind ClusterBuildPlane")
+			err := k8sClient.Get(ctx, typeNamespacedName, clusterbuildplane)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &openchoreov1alpha1.ClusterBuildPlane{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: resourceName,
+					},
+					Spec: openchoreov1alpha1.ClusterBuildPlaneSpec{
+						PlaneID: "test-plane",
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &openchoreov1alpha1.ClusterBuildPlane{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance ClusterBuildPlane")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &Reconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})

internal/controller/clusterdataplane/controller_test.go
@@ -1,0 +1,72 @@
+// Copyright 2025 The OpenChoreo Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package clusterdataplane
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	openchoreov1alpha1 "github.com/openchoreo/openchoreo/api/v1alpha1"
+)
+
+var _ = Describe("ClusterDataPlane Controller", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name: resourceName,
+			// No namespace for cluster-scoped resources
+		}
+		clusterdataplane := &openchoreov1alpha1.ClusterDataPlane{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind ClusterDataPlane")
+			err := k8sClient.Get(ctx, typeNamespacedName, clusterdataplane)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &openchoreov1alpha1.ClusterDataPlane{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: resourceName,
+						// No namespace for cluster-scoped resources
+					},
+					Spec: openchoreov1alpha1.ClusterDataPlaneSpec{
+						PlaneID: "test-plane",
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &openchoreov1alpha1.ClusterDataPlane{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance ClusterDataPlane")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &Reconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})

internal/controller/clusterobservabilityplane/controller_test.go
@@ -1,0 +1,72 @@
+// Copyright 2025 The OpenChoreo Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package clusterobservabilityplane
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	openchoreov1alpha1 "github.com/openchoreo/openchoreo/api/v1alpha1"
+)
+
+var _ = Describe("ClusterObservabilityPlane Controller", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name: resourceName,
+			// No namespace for cluster-scoped resources
+		}
+		clusterobservabilityplane := &openchoreov1alpha1.ClusterObservabilityPlane{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind ClusterObservabilityPlane")
+			err := k8sClient.Get(ctx, typeNamespacedName, clusterobservabilityplane)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &openchoreov1alpha1.ClusterObservabilityPlane{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: resourceName,
+						// No namespace for cluster-scoped resources
+					},
+					Spec: openchoreov1alpha1.ClusterObservabilityPlaneSpec{
+						PlaneID: "test-plane",
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &openchoreov1alpha1.ClusterObservabilityPlane{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance ClusterObservabilityPlane")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &Reconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})