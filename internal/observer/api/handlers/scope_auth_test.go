5dd002e8462c25dbaae04c5df17cd959385363da
Nilushan Costa <nilushancosta@gmail.com>
2026-09-03 14:01:13 +0530

    Revert "refactor: replace GET method API in Observer used to fetch span detai…"
    This reverts commit a65229baa14f7751ef234eae38229c275a45c988.

--- internal/observer/api/handlers/scope_auth_test.go
+++ internal/observer/api/handlers/scope_auth_test.go
@@ -191,23 +191,23 @@ func TestQuerySpans_ScopeAuthFailed_Returns500WithCode(t *testing.T) {
 	assertScopeAuthFailedResponse(t, rr)
 }
 
-func TestQuerySpanDetails_ScopeAuthFailed_Returns500WithCode(t *testing.T) {
+func TestGetSpanDetails_ScopeAuthFailed_Returns500WithCode(t *testing.T) {
 	t.Parallel()
 
 	svc := servicemocks.NewMockTracesQuerier(t)
-	svc.On("QuerySpanDetails", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil, fmt.Errorf("%w: token expired after idle", service.ErrScopeAuthFailed))
+	svc.On("GetSpanDetails", mock.Anything, mock.Anything, mock.Anything).Return(nil, fmt.Errorf("%w: token expired after idle", service.ErrScopeAuthFailed))
 
 	h := &Handler{
 		baseHandler:   baseHandler{logger: noopLogger()},
 		tracesService: svc,
 	}
 
-	req := httptest.NewRequest(http.MethodPost, "/api/v1alpha1/traces/trace-1/spans/span-1", spanDetailsBody("test-ns"))
+	req := httptest.NewRequest(http.MethodGet, "/api/v1alpha1/traces/trace-1/spans/span-1", nil)
 	req.SetPathValue("traceId", "trace-1")
 	req.SetPathValue("spanId", "span-1")
 	rr := httptest.NewRecorder()
 
-	h.QuerySpanDetailsForTrace(rr, req)
+	h.GetSpanDetailsForTrace(rr, req)
 
 	assertScopeAuthFailedResponse(t, rr)
 }
